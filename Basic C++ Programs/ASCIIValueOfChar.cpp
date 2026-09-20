e7b6bd11fc30e93faa8ebc80e573b60713a1cafb
Shekhar Gupta <shekhargupta2307@gmail.com>
2026-09-20 19:08:38 +0530

    Add files via upload

---
+++ Basic C++ Programs/ASCIIValueOfChar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char ch = 'A';
+
+    cout << "Character: " << ch << endl;
+    cout << "ASCII Value: " << int(ch);
+
+    return 0;
+}
